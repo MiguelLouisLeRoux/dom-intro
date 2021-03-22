fa6260ae691dd8f6efe415141b9df2bef217736f
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-03-22 17:04:46 +0200

    Completed calculate-bill widget

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,8 +1,14 @@
-//get a reference to the calculate button
+var calculateBtnn = document.querySelector(".calculateBtn")//get a reference to the calculate button
+var billTotalElement = document.querySelector(".billTotal");//get a reference to the billTotal element
+var billStr = document.querySelector(".billString");//get a reference to the billString
+// var billIn = billStr.value;
 
-//get a reference to the billTotal element
+// function val() {
+//     var n = document.querySelector(".billString").value;
+//     alert(n);
+// }
 
-//get a reference to the billString
+// calculateBtnn.addEventListener('click', val);
 
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
@@ -10,4 +16,43 @@
 //  * check if it is a call or an sms and add the right amount to the overall total
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
+
+function totalPhoneBill() {
+    var billStringVal = billStr.value;
+    var arr = billStringVal.split(",");
+    var callArr = [];
+    var smsArr = [];
+    
+    for (var i = 0; i < arr.length; i++) {
+      var newArr = arr[i];
+      var split = newArr.trim();
+      if (split.startsWith("call")) {
+        callArr.push(split);
+      } else if (split.startsWith("sms")) {
+        smsArr.push(split);
+      }
+      
+    }
+    var newCallArr = callArr.length;
+    var newSmsArr = smsArr.length;
+    
+    var total = newCallArr * 2.75 + newSmsArr * 0.75;
+    var grandTotal = total.toFixed(2);
+    // return grandTotal;
+    
+    billTotalElement.innerHTML = grandTotal;
+    
+    if (grandTotal <= 20) {
+        document.querySelector(".total").style.color = "black";
+     } else if (grandTotal > 20 && grandTotal < 30) {
+        document.querySelector(".total").style.color = "orange"; 
+     } else if (grandTotal > 30) {
+        document.querySelector(".total").style.color = "crimson";
+     }
+}   
+
+
+
+
 //link the function to a click event on the calculate button
+calculateBtnn.addEventListener('click', totalPhoneBill);
