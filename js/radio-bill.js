2244de80e6588ae430780531fa22e5e9a9163972
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-03-23 16:48:39 +0200

    completed radio bill widget

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,8 +1,10 @@
-// get a reference to the sms or call radio buttons
-
-//get a reference to the add button
-
+// var radio = document.querySelector("input[name='billItemType']");// get a reference to the sms or call radio buttons
+var addButton2 = document.querySelector(".radioBillAddBtn");//get a reference to the add button
+var callTotal2 = document.querySelector(".callTotalTwo");
+var smsTotal2 = document.querySelector(".smsTotalTwo");
+var gTotal2 = document.querySelector(".totalTwo");
 //create a variable that will keep track of the total bill
+
 
 //add an event listener for when the add button is pressed
 
@@ -10,3 +12,59 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+
+var callArrRadio = [];
+var smsArrRadio = [];
+
+function radioBill() {
+    var radio = document.querySelector("input[name='billItemType']:checked");
+
+    if (radio) {
+        var radVal = radio.value;
+        
+        if (radio.value === 'call') {
+
+            callArrRadio.push(radio.value);
+        } else if (radio.value === 'sms') {
+    
+            smsArrRadio.push(radio.value);
+        }
+ 
+    }
+
+    // if (radio.value == 'call') {
+
+    //     callArrRadio.push(radio.value);
+    // } else if (radio.value == 'sms') {
+
+    //     smsArrRadio.push(radio.value);
+    // }
+
+    var newCallArr = callArrRadio.length;
+    var newSmsArr = smsArrRadio.length;
+
+    //call total calculation
+    var callTotal = newCallArr * 2.75;
+    callTotal2.innerHTML = callTotal;
+
+    //sms total calculation
+    var smsTotal = newSmsArr * 0.75;
+    smsTotal2.innerHTML = smsTotal
+    
+    var total = newCallArr * 2.75 + newSmsArr * 0.75;
+    var gTotal = total.toFixed(2);
+
+    // return grandTotal;
+    gTotal2.innerHTML = gTotal;
+    
+    if (grandTotal < 30) {
+        document.querySelector(".orange").style.color = "black";
+
+     } else if (gTotal >= 30 && gTotal < 50) {
+        document.querySelector(".orange").style.color = "orange"; 
+     } else if (gTotal >= 50) {
+        document.querySelector(".orange").style.color = "crimson";
+     }
+}
+
+addButton2.addEventListener('click', radioBill);
--- js/text-bill.js
+++ js/text-bill.js
@@ -1,12 +1,67 @@
-// get a reference to the textbox where the bill type is to be entered
-
-//get a reference to the add button
+var textIn = document.querySelector(".billTypeText"); // get a reference to the textbox where the bill type is to be entered
+var addBtn = document.querySelector(".addToBillBtn"); //get a reference to the add button
+var callTot = document.querySelector(".callTotalOne");
+var smsTot = document.querySelector(".smsTotalOne");
+var grandTotal = document.querySelector(".totalOne");
 
 //create a variable that will keep track of the total bill
-
 //add an event listener for when the add button is pressed
-
 //in the event listener check if the value in the bill type textbox is 'sms' or 'call'
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+var callArr = [];
+var smsArr = [];
+
+function PhoneBill() {
+    var input = textIn.value;
+    var trimIn = input.trim();
+    // var arr = billStringVal.split(",");
+    // var callArr = [];
+    // var smsArr = [];
+    
+    // for (var i = 0; i < arr.length; i++) {
+    //   var newArr = arr[i];
+    //   var split = newArr.trim();
+    //   if (split.startsWith("call")) {
+    //     callArr.push(split);
+    //   } else if (split.startsWith("sms")) {
+    //     smsArr.push(split);
+    //   }
+      
+    // }
+
+    if (trimIn === "call") {
+        callArr.push(trimIn);
+    } else if (trimIn === "sms") {
+        smsArr.push(trimIn);
+    }
+
+    var newCallArr = callArr.length;
+    var newSmsArr = smsArr.length;
+
+    //call total calculation
+    var callTotal = newCallArr * 2.75;
+    callTot.innerHTML = callTotal;
+
+    //sms total calculation
+    var smsTotal = newSmsArr * 0.75;
+    smsTot.innerHTML = smsTotal
+    
+    var total = newCallArr * 2.75 + newSmsArr * 0.75;
+    var gTotal = total.toFixed(2);
+    // return grandTotal;
+    
+    grandTotal.innerHTML = gTotal;
+    
+    if (gTotal < 30) {
+        document.querySelector(".red").style.color = "black";
+
+     } else if (gTotal >= 30 && gTotal < 50) {
+        document.querySelector(".red").style.color = "orange"; 
+     } else if (gTotal >= 50) {
+        document.querySelector(".red").style.color = "crimson";
+     }
+}
+
+addBtn.addEventListener('click', PhoneBill);
